251bb9a74fd428648b210d7c8585b57996c66fe7
Jo-hsi TAN <jo-tan@e2r2p6.clusters.42paris.fr>
2023-02-19 03:30:43 +0100

    add ft_split. 19 Feb 2023

---
+++ ft_split/ft_split.c
@@ -0,0 +1,110 @@
+/*
+ * Assignment name  : ft_split
+ * Expected files   : ft_split.c
+ * Allowed functions: malloc
+ * ---------------------------------------------------------------------
+ *
+ * Write a function that takes a string, splits it into words,
+ * and returns them as a NULL-terminated array of strings.
+ *
+ * A "word" is defined as a part of a string delimited either by spaces/tabs/newlines,
+ * or by the start/end of the string.
+ *
+ * Your function must be declared as follows:
+ *
+ * char    **ft_split(char *str);
+ * */
+
+#include <stdlib.h>
+
+int	ft_count_str(char const *s)
+{
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+	if (!s[i])
+		return (0);
+	if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n')
+		i++;
+	while (s[i])
+	{
+		if ((s[i] == ' ' || s[i] == '\t' || s[i] == '\n') && (s[i - 1] != ' ' && s[i - 1] != '\t' && s[i - 1] != '\n'))
+			count++;
+		i++;
+	}
+	if (s[i] == '\0' && (s[i - 1] != ' ' && s[i - 1] != '\t' && s[i - 1] != '\n'))
+		count++;
+	return (count);
+}
+
+int	ft_str_len(char const *s, int i)
+{
+	int	len;
+
+	len = 0;
+	while (s[i] && (s[i] != ' ' && s[i] != '\t' && s[i] != '\n'))
+	{
+		len++;
+		i++;
+	}
+	return (len);
+}
+
+char	*ft_write_string(char const *s, char **ptr, int j, int len)
+{
+	int		x;
+	char	*p;
+
+	x = 0;
+	p = (char *)s;
+	ptr[j] = malloc((len + 1) * sizeof(char));
+	if (!ptr[j])
+	{
+		while (j != 0)
+		{
+			free(ptr[j]);
+			j--;
+		}
+		free(ptr);
+		return (NULL);
+	}
+	while (len != 0)
+	{
+		ptr[j][x] = p[x];
+		x++;
+		len--;
+	}
+	ptr[j][x] = '\0';
+	return (ptr[j]);
+}
+
+char	**ft_split(char const *s)
+{
+	char	**ptr;
+	int		i;
+	int		j;
+	int		len;
+
+	i = 0;
+	j = 0;
+	ptr = malloc((ft_count_str(s) + 1) * sizeof(char *));
+	if (!ptr)
+		return (NULL);
+	while (s[i])
+	{
+		if (s[i] != ' ' && s[i] != '\t' && s[i] != '\n')
+		{
+			len = ft_str_len(s, i);
+			ptr[j] = ft_write_string(s + i, ptr, j, len);
+			j++;
+			i += len;
+			if (s[i] == '\0')
+				break ;
+		}
+		i++;
+	}
+	ptr[j] = NULL;
+	return (ptr);
+}
